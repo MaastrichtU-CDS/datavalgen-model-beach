eb7c2233e2deb10a929b594f8fb4d5965bde4d71
Luis Sago <lws.sago@gmail.com>
2026-02-19 13:25:27 +0100

    Add link to nsclc staging info

--- src/datavalgen_model_beach/model.py
+++ src/datavalgen_model_beach/model.py
@@ -25,10 +25,10 @@ class DataModel(BaseModel):
         ..., description="M stage"
     )
 
-    # TODO: some more description here?
+    # See: https://www.cancer.org/cancer/types/lung-cancer/detection-diagnosis-staging/staging-nsclc.html
     patient_overall_stage: Literal[
-        "x",
-        "0",
+        "x",       # hidden (occult) cancer
+        "0",       # stage 0
         "IA1",
         "IA2",
         "IA3",
